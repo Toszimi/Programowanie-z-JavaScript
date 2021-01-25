aae8f95a8c814e3856157e0dbe77df475cf1619d
Damian Machaty <wlochaty123@poczta.fm>
2021-01-25 22:32:42 +0100

    działa

--- Lightbox/main.js
+++ Lightbox/main.js
@@ -1,23 +1,38 @@
 const lightbox = document.querySelector('.lightbox');
 const Zdjecia = document.querySelectorAll('.galeria img');
-for (let idx = 0; idx < Zdjecia.length; idx++) {
-    const img = Zdjecia[idx];
+const poprzedni = document.querySelector('#poprzedniBtn');
+const nastepny = document.querySelector('#nastepnyBtn');
+const zamknij = document.querySelector('#zamknij');
+
+let poprzedniElement;
+let nastepnyElement;
+
+for (let index = 0; index < Zdjecia.length; index++) {
+    const img = Zdjecia[index];
     img.addEventListener('click', PokazLightbox);
 }
-
 
 function PokazLightbox(ev) {
     const lightbox = document.querySelector('.lightbox');
     const Zdj = document.querySelector('.lightbox img');
     const ZdjUrl = ev.target.src;
     Zdj.src = ZdjUrl;
-    console.log('showlight');
     lightbox.classList.add('widoczne');
-
+    poprzedniElement = ev.target.previousElementSibling;
+    nastepnyElement = ev.target.nextElementSibling;
 }
 
-lightbox.addEventListener('click', ukryjLightbox);
+zamknij.addEventListener('click', ukryjLightbox);
 
 function ukryjLightbox(){
     lightbox.classList.remove('widoczne');
 }
+nastepny.addEventListener('click', function () {
+    ukryjLightbox();
+    nastepnyElement ? nastepnyElement.click() : Zdjecia[0].click();
+});
+
+poprzedni.addEventListener('click', function () {
+    ukryjLightbox();
+    poprzedniElement ? poprzedniElement.click() : Zdjecia[Zdjecia.length - 1].click();
+});
